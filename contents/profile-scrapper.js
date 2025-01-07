89e9fbe2dec69fde121f86f8f8969c06d6001dcf
Jacob Krafft <Jacob.Krafft@gmail.com>
2025-01-07 18:58:29 +0100

    changed selection of primaryColumn to fix profil data collection

--- contents/profile-scrapper.js
+++ contents/profile-scrapper.js
@@ -1,82 +1,112 @@
-export{}
+export {}
 
 export async function profileScrape() {
-  console.log("Running the profile page scraper");
+  console.log("Running the profile page scraper")
 
   const scrapeData = () => {
     return new Promise((resolve) => {
-      const maxAttempts = 10;
-      let attempts = 0;
+      const maxAttempts = 10
+      let attempts = 0
 
       const attemptScraping = () => {
-        attempts++;
-        console.log(`Attempt ${attempts}: Checking for elements...`);
+        attempts++
+        console.log(`Attempt ${attempts}: Checking for elements...`)
 
         // Check if the main element is present
-        const main = document.querySelector('main[role="main"]');
+        const main = document.querySelector('main[role="main"]')
         if (!main) {
-          console.log("Main element not found, retrying...");
-          retryOrResolve();
-          return;
+          console.log("Main element not found, retrying...")
+          retryOrResolve()
+          return
         }
 
         console.log("main>>>>>>>>>>", main)
         // Check if the profileBio element is present
-        const primaryColoumn = main.querySelector(':scope > div > div > div > div[data-testid="primaryColumn"] > div[aria-label="Home timeline"] > div:nth-child(3)');
+        const primaryColoumn = main.querySelector(
+          'div[data-testid="primaryColumn"] > div[aria-label^="Home"] > div:nth-child(3)'
+        )
         console.log("primaryColoumn", primaryColoumn)
         if (!primaryColoumn) {
-          console.log("Main element not found, retrying...");
-          retryOrResolve();
-          return;
+          console.log("Main element not found, retrying...")
+          retryOrResolve()
+          return
         }
 
-        const profileBio = primaryColoumn.querySelector(':scope > div > div > div > div')
+        const profileBio = primaryColoumn.querySelector(
+          ":scope > div > div > div > div"
+        )
         if (!profileBio) {
-          console.log("Profile bio not found, retrying...");
-          retryOrResolve();
-          return;
+          console.log("Profile bio not found, retrying...")
+          retryOrResolve()
+          return
         }
 
         console.log("profileBio>>>>>", profileBio)
         // Select additional elements conditionally
-        const thirddivbio = profileBio.querySelector(":scope > div:nth-child(3)");
-        console.log("thirddivbio:", thirddivbio);
-        
-        const otherbiodetails = profileBio.querySelector(":scope > div:nth-child(4)");
-        console.log("otherbiodetails:", otherbiodetails);
-        
-        const following_followercount = profileBio.querySelector(":scope > div:nth-child(5)");
-        console.log("following_followercount:", following_followercount);
-        
-        const followingCount = following_followercount?.querySelector('div > a[href*="following"]')?.innerText || " ";
-        console.log("followingCount:", followingCount);
-        
-        const followersCount = following_followercount?.querySelector('div > a[href*="verified_followers"]')?.innerText || " ";
-        console.log("followersCount:", followersCount);
-        
+        const thirddivbio = profileBio.querySelector(
+          ":scope > div:nth-child(3)"
+        )
+        console.log("thirddivbio:", thirddivbio)
+
+        const otherbiodetails = profileBio.querySelector(
+          ":scope > div:nth-child(4)"
+        )
+        console.log("otherbiodetails:", otherbiodetails)
+
+        const following_followercount = profileBio.querySelector(
+          ":scope > div:nth-child(5)"
+        )
+        console.log("following_followercount:", following_followercount)
+
+        const followingCount =
+          following_followercount?.querySelector('div > a[href*="following"]')
+            ?.innerText || " "
+        console.log("followingCount:", followingCount)
+
+        const followersCount =
+          following_followercount?.querySelector(
+            'div > a[href*="verified_followers"]'
+          )?.innerText || " "
+        console.log("followersCount:", followersCount)
+
         let profilebiodata
-        const profileBioofUser = thirddivbio?.querySelector(':scope > div > div[data-testid="UserDescription"]')
-        console.log("profileBioofUser:", profileBioofUser);
+        const profileBioofUser = thirddivbio?.querySelector(
+          ':scope > div > div[data-testid="UserDescription"]'
+        )
+        console.log("profileBioofUser:", profileBioofUser)
         if (profileBioofUser) {
-          const profiledata = profileBioofUser.querySelectorAll(':scope > span');
-          profilebiodata = Array.from(profiledata)
-              .map(span => span.textContent.trim())
-              .join(" ") || "";
-          console.log("profilebiodata:", profilebiodata);
-      } else {
-          console.log("profilebiodata: Data not found");
-      }
-        
-        const userlocation = otherbiodetails?.querySelector(':scope > div[data-testid="UserProfileHeader_Items"] > span[data-testid="UserLocation"]')?.innerText || "";
-        console.log("userlocation:", userlocation);
+          const profiledata = profileBioofUser.querySelectorAll(":scope > span")
+          profilebiodata =
+            Array.from(profiledata)
+              .map((span) => span.textContent.trim())
+              .join(" ") || ""
+          console.log("profilebiodata:", profilebiodata)
+        } else {
+          console.log("profilebiodata: Data not found")
+        }
 
-        const userBirthdate = otherbiodetails?.querySelector(':scope > div[data-testid="UserProfileHeader_Items"] > span[data-testid="UserBirthdate"]')?.innerText || "";
-        console.log("userBirthdate:", userBirthdate);
-        
-        const userJoindate = otherbiodetails?.querySelector(':scope > div[data-testid="UserProfileHeader_Items"] > span[data-testid="UserJoinDate"]')?.innerText || "";
-        console.log("userJoindate:", userJoindate);
+        const userlocation =
+          otherbiodetails?.querySelector(
+            ':scope > div[data-testid="UserProfileHeader_Items"] > span[data-testid="UserLocation"]'
+          )?.innerText || ""
+        console.log("userlocation:", userlocation)
 
-        const userUrl = otherbiodetails?.querySelector(':scope > div[data-testid="UserProfileHeader_Items"] > a[data-testid="UserUrl"]')?.innerText || "";
+        const userBirthdate =
+          otherbiodetails?.querySelector(
+            ':scope > div[data-testid="UserProfileHeader_Items"] > span[data-testid="UserBirthdate"]'
+          )?.innerText || ""
+        console.log("userBirthdate:", userBirthdate)
+
+        const userJoindate =
+          otherbiodetails?.querySelector(
+            ':scope > div[data-testid="UserProfileHeader_Items"] > span[data-testid="UserJoinDate"]'
+          )?.innerText || ""
+        console.log("userJoindate:", userJoindate)
+
+        const userUrl =
+          otherbiodetails?.querySelector(
+            ':scope > div[data-testid="UserProfileHeader_Items"] > a[data-testid="UserUrl"]'
+          )?.innerText || ""
         console.log("userUrl>>>>>>>", userUrl)
         //screenname scrape
         // Check if necessary data has been found
@@ -92,18 +122,18 @@ export async function profileScrape() {
             userlocation,
             userBirthdate,
             userUrl
-          };
-          console.log("Scraped data:", scrapedData);
-          resolve(scrapedData);
-          return;
+          }
+          console.log("Scraped data:", scrapedData)
+          resolve(scrapedData)
+          return
         }
 
-        retryOrResolve();
-      };
+        retryOrResolve()
+      }
 
       const retryOrResolve = () => {
         if (attempts >= maxAttempts) {
-          console.log("Max attempts reached without finding all data.");
+          console.log("Max attempts reached without finding all data.")
           resolve({
             profileBioofUser: "",
             userJoindate: "",
@@ -111,24 +141,23 @@ export async function profileScrape() {
             followingCount: "",
             userlocation: "",
             userBirthdate: ""
-          });
+          })
         } else {
-          setTimeout(attemptScraping, 1000);
+          setTimeout(attemptScraping, 1000)
         }
-      };
+      }
 
-      attemptScraping();
-    });
-  };
+      attemptScraping()
+    })
+  }
 
   if (document.readyState === "complete") {
-    return await scrapeData();
+    return await scrapeData()
   } else {
     return new Promise((resolve) => {
       window.addEventListener("load", async () => {
-        resolve(await scrapeData());
-      });
-    });
+        resolve(await scrapeData())
+      })
+    })
   }
 }
-
